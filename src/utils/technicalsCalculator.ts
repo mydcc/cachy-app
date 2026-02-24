560b2e14ef93684ec565c84def531f8057b7e319
mydcc <pheinze82@gmail.com>
2026-02-24 11:42:39 +0000

    Fix Parabolic SAR to use increment setting instead of ignoring it

--- src/utils/technicalsCalculator.ts
+++ src/utils/technicalsCalculator.ts
@@ -432,9 +432,11 @@ export function calculateIndicatorsFromArrays(
 
       if (shouldCalculate('parabolicSar')) {
           const start = settings?.parabolicSar?.start || 0.02;
+          const increment = settings?.parabolicSar?.increment || 0.02;
           const max = settings?.parabolicSar?.max || 0.2;
-          const res = JSIndicators.psar(highsNum, lowsNum, start, max);
+          const res = JSIndicators.psar(highsNum, lowsNum, increment, max);
           advancedInfo.parabolicSar = res[res.length - 1];
+      }
       }
 
   } catch (e) {
