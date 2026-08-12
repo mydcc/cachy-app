17ff6d5a50e5d506dba1dafd59473308594a8422
mydcc <pheinze82@gmail.com>
2026-08-12 23:39:32 +0200

    chore(repair): remove temporary debug script fix.cjs

--- fix.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const file = 'src/services/dataRepairService.ts';
-let code = fs.readFileSync(file, 'utf-8');
-
-// The linter error is on line 293. Let's find out what it is.
