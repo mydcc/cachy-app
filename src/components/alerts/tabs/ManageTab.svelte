a88810908beceb336a7f48ff05b035d44e4c5c0c
mydcc <23080170+mydcc@users.noreply.github.com>
2026-09-06 22:22:09 +0200

    fix: remove duplicate symbol prop from ManageTab

--- src/components/alerts/tabs/ManageTab.svelte
+++ src/components/alerts/tabs/ManageTab.svelte
@@ -31,7 +31,6 @@
 -->
 
 <script lang="ts">
-    let { symbol: _symbol }: { symbol: string } = $props();
     import { alertState } from "../../../stores/alerts.svelte";
     import { _ } from "../../../locales/i18n";
     import { generateId } from "../../../utils/utils";
