d27defeda1879966bf08368f7306f177856bf9d8
mydcc <23080170+mydcc@users.noreply.github.com>
2026-09-10 14:59:53 +0200

    test(technicals): simplify self-import in stateful calculator test

--- src/utils/statefulTechnicalsCalculator.test.ts
+++ src/utils/statefulTechnicalsCalculator.test.ts
@@ -18,7 +18,7 @@
 
 
 import { describe, it, expect } from 'vitest';
-import { StatefulTechnicalsCalculator } from '../../src/utils/statefulTechnicalsCalculator';
+import { StatefulTechnicalsCalculator } from './statefulTechnicalsCalculator';
 import { Decimal } from 'decimal.js';
 
 function mockKline(time: number, close: number) {
